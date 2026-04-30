40928854e0fc9e13debb71ef865c253f65887293
Hugh O'Neill <hugh.j.oneill.4@gmail.com>
2026-04-30 07:28:41 -0400

    gen9 OU sample teams (6 archetypes: sun, BO, balance, stall, rain, TR)
    Sourced from Smogon's official SV OU Sample Teams thread (post Tera Blast
    ban, April 2026). One representative team per major archetype to give the
    A/B bench coverage across the meta:
    
      0 - Sun Offense (Venusaur + Walking Wake)
      1 - Bulky Offense (Garganacl + Hatterene)
      2 - Balance (Gholdengo + Ting-Lu hazard stack)
      3 - Stall (Dondozo + Blissey + Toxapex)
      4 - Rain (Pelipper + Barraskewda + Archaludon)
      5 - Trick Room (Hoopa-U + Ursaluna + dual setters)
    
    The gen9 build_pe_state path doesn't exist yet — these teams aren't loaded
    into the bench until that wiring lands.

---
+++ showdown/sample_teams_gen9.py
@@ -0,0 +1,391 @@
+# Gen 9 OU sample teams from Smogon's official sample teams thread (post Tera Blast ban, April 2026).
+# source: https://www.smogon.com/forums/threads/sv-ou-sample-teams-new-samples-added-post-scl-and-olt.3712513/
+# Each team is a different archetype to give the bench coverage across the meta.
+
+SAMPLE_TEAMS_GEN9 = [
+    # ---- 0: Sun Offense (Venusaur + Walking Wake) ----
+    """Great Tusk @ Assault Vest
+Ability: Protosynthesis
+Tera Type: Water
+EVs: 160 HP / 132 Atk / 12 SpD / 204 Spe
+Adamant Nature
+- Headlong Rush
+- Rapid Spin
+- Ice Spinner
+- Close Combat
+
+Walking Wake @ Wise Glasses
+Ability: Protosynthesis
+Tera Type: Ghost
+EVs: 12 HP / 244 SpA / 252 Spe
+Timid Nature
+- Hydro Steam
+- Draco Meteor
+- Flamethrower
+- Flip Turn
+
+Kingambit @ Air Balloon
+Ability: Supreme Overlord
+Tera Type: Ghost
+EVs: 140 HP / 252 Atk / 112 Spe
+Adamant Nature
+- Swords Dance
+- Kowtow Cleave
+- Iron Head
+- Sucker Punch
+
+Venusaur @ Life Orb
+Ability: Chlorophyll
+Tera Type: Fire
+EVs: 252 SpA / 4 SpD / 252 Spe
+Timid Nature
+IVs: 0 Atk
+- Growth
+- Giga Drain
+- Weather Ball
+- Sludge Bomb
+
+Ninetales @ Heat Rock
+Ability: Drought
+Tera Type: Ghost
+EVs: 248 HP / 44 Def / 216 Spe
+Timid Nature
+IVs: 0 Atk
+- Will-O-Wisp
+- Weather Ball
+- Healing Wish
+- Encore
+
+Raging Bolt @ Air Balloon
+Ability: Protosynthesis
+Tera Type: Fairy
+EVs: 4 HP / 252 SpA / 252 Spe
+Modest Nature
+IVs: 20 Atk
+- Thunderbolt
+- Thunderclap
+- Dragon Pulse
+- Calm Mind""",
+
+    # ---- 1: Bulky Offense (Garganacl + Hatterene) ----
+    """Garganacl @ Leftovers
+Ability: Purifying Salt
+Tera Type: Water
+EVs: 252 HP / 16 Def / 216 SpD / 24 Spe
+Careful Nature
+- Salt Cure
+- Stealth Rock
+- Protect
+- Recover
+
+Darkrai @ Choice Specs
+Ability: Bad Dreams
+Tera Type: Poison
+EVs: 252 SpA / 4 SpD / 252 Spe
+Timid Nature
+IVs: 0 Atk
+- Dark Pulse
+- Psychic
+- Focus Blast
+- Sludge Bomb
+
+Great Tusk @ Booster Energy
+Ability: Protosynthesis
+Tera Type: Ice
+EVs: 252 Atk / 4 Def / 252 Spe
+Jolly Nature
+- Headlong Rush
+- Close Combat
+- Ice Spinner
+- Rapid Spin
+
+Hatterene @ Rocky Helmet
+Ability: Magic Bounce
+Tera Type: Flying
+EVs: 240 HP / 252 Def / 16 SpA
+Relaxed Nature
+IVs: 0 Spe
+- Dazzling Gleam
+- Future Sight
+- Nuzzle
+- Pain Split
+
+Tornadus-Therian @ Assault Vest
+Ability: Regenerator
+Tera Type: Fairy
+EVs: 184 HP / 16 Def / 68 SpD / 240 Spe
+Timid Nature
+- Bleakwind Storm
+- Knock Off
+- U-turn
+- Heat Wave
+
+Dragonite @ Silk Scarf
+Ability: Multiscale
+Tera Type: Normal
+EVs: 152 HP / 252 Atk / 104 Spe
+Adamant Nature
+- Dragon Dance
+- Roost
+- Extreme Speed
+- Earthquake""",
+
+    # ---- 2: Balance (Gholdengo + Ting-Lu hazard stack) ----
+    """Gholdengo @ Choice Scarf
+Ability: Good as Gold
+Tera Type: Steel
+EVs: 4 Def / 252 SpA / 252 Spe
+Timid Nature
+IVs: 0 Atk
+- Shadow Ball
+- Make It Rain
+- Thunder Wave
+- Trick
+
+Darkrai @ Heavy-Duty Boots
+Ability: Bad Dreams
+Tera Type: Ghost
+EVs: 4 HP / 252 SpA / 252 Spe
+Timid Nature
+- Dark Pulse
+- Ice Beam
+- Knock Off
+- Thunder Wave
+
+Clefable @ Life Orb
+Ability: Magic Guard
+Tera Type: Water
+EVs: 252 HP / 80 SpA / 176 Spe
+Modest Nature
+IVs: 0 Atk
+- Calm Mind
+- Moonblast
+- Flamethrower
+- Moonlight
+
+Ting-Lu @ Red Card
+Ability: Vessel of Ruin
+Tera Type: Ghost
+EVs: 252 HP / 4 Def / 252 SpD
+Relaxed Nature
+IVs: 0 Atk / 0 Spe
+- Stealth Rock
+- Spikes
+- Ruination
+- Whirlwind
+
+Dragonite @ Heavy-Duty Boots
+Ability: Multiscale
+Tera Type: Normal
+EVs: 4 HP / 252 Atk / 252 Spe
+Adamant Nature
+- Dragon Dance
+- Extreme Speed
+- Earthquake
+- Roost
+
+Pecharunt @ Heavy-Duty Boots
+Ability: Poison Puppeteer
+Tera Type: Ghost
+EVs: 252 HP / 4 SpA / 252 Spe
+Timid Nature
+IVs: 0 Atk
+- Nasty Plot
+- Shadow Ball
+- Malignant Chain
+- Recover""",
+
+    # ---- 3: Stall (Dondozo + Blissey + Toxapex) ----
+    """Dondozo @ Leftovers
+Ability: Unaware
+Tera Type: Dragon
+EVs: 248 HP / 252 Def / 8 Spe
+Impish Nature
+- Body Press
+- Avalanche
+- Rest
+- Sleep Talk
+
+Blissey @ Leftovers
+Ability: Natural Cure
+Tera Type: Dark
+EVs: 20 HP / 252 Def / 236 SpD
+Calm Nature
+IVs: 0 Atk
+- Seismic Toss
+- Soft-Boiled
+- Calm Mind
+- Stealth Rock
+
+Corviknight @ Leftovers
+Ability: Pressure
+Tera Type: Fighting
+EVs: 252 HP / 252 Def / 4 SpD
+Impish Nature
+IVs: 0 Atk
+- Iron Defense
+- Defog
+- Roost
+- Body Press
+
+Weezing-Galar @ Heavy-Duty Boots
+Ability: Neutralizing Gas
+Tera Type: Ghost
+EVs: 252 HP / 244 Def / 12 Spe
+Bold Nature
+IVs: 0 Atk
+- Will-O-Wisp
+- Defog
+- Pain Split
+- Toxic Spikes
+
+Gliscor @ Toxic Orb
+Ability: Poison Heal
+Tera Type: Water
+EVs: 244 HP / 12 Def / 252 SpD
+Careful Nature
+IVs: 24 Spe
+- Earthquake
+- Spikes
+- Knock Off
+- Protect
+
+Toxapex @ Leftovers
+Ability: Regenerator
+Tera Type: Steel
+EVs: 252 HP / 252 SpD / 4 Spe
+Calm Nature
+- Toxic
+- Recover
+- Poison Jab
+- Haze""",
+
+    # ---- 4: Rain (Pelipper + Barraskewda + Archaludon) ----
+    """Pelipper @ Damp Rock
+Ability: Drizzle
+Tera Type: Water
+EVs: 248 HP / 48 Def / 212 SpD
+Relaxed Nature
+IVs: 0 Spe
+- Surf
+- U-turn
+- Hurricane
+- Roost
+
+Barraskewda @ Choice Band
+Ability: Swift Swim
+Tera Type: Water
+EVs: 252 Atk / 4 SpD / 252 Spe
+Adamant Nature
+- Liquidation
+- Flip Turn
+- Aqua Jet
+- Close Combat
+
+Kingambit @ Air Balloon
+Ability: Supreme Overlord
+Tera Type: Fairy
+EVs: 252 Atk / 4 Def / 252 Spe
+Jolly Nature
+- Swords Dance
+- Sucker Punch
+- Kowtow Cleave
+- Tera Blast
+
+Archaludon @ Assault Vest
+Ability: Stamina
+Tera Type: Fairy
+EVs: 252 HP / 176 SpA / 8 SpD / 72 Spe
+Modest Nature
+IVs: 0 Atk
+- Draco Meteor
+- Flash Cannon
+- Electro Shot
+- Body Press
+
+Iron Treads @ Eject Button
+Ability: Quark Drive
+Tera Type: Ghost
+EVs: 120 HP / 252 SpA / 136 Spe
+Timid Nature
+- Earth Power
+- Rapid Spin
+- Stealth Rock
+- Volt Switch
+
+Raging Bolt @ Booster Energy
+Ability: Protosynthesis
+Tera Type: Fairy
+EVs: 196 HP / 252 SpA / 60 Spe
+Modest Nature
+IVs: 20 Atk
+- Calm Mind
+- Thunderclap
+- Weather Ball
+- Dragon Pulse""",
+
+    # ---- 5: Trick Room (Hoopa-U + Ursaluna + dual setters) ----
+    """Hoopa-Unbound @ Eject Pack
+Ability: Magician
+Tera Type: Dark
+EVs: 248 HP / 252 Atk / 8 SpA
+Brave Nature
+IVs: 0 Spe
+- Hyperspace Fury
+- Drain Punch
+- Psychic
+- Trick Room
+
+Ursaluna @ Flame Orb
+Ability: Guts
+Tera Type: Normal
+EVs: 252 HP / 252 Atk / 4 SpD
+Brave Nature
+IVs: 0 Spe
+- Facade
+- Headlong Rush
+- Fire Punch
+- Swords Dance
+
+Hatterene @ Focus Sash
+Ability: Magic Bounce
+Tera Type: Ghost
+EVs: 252 HP / 4 Def / 252 SpA
+Quiet Nature
+IVs: 0 Atk / 0 Spe
+- Dazzling Gleam
+- Psychic
+- Trick Room
+- Healing Wish
+
+Cresselia @ Leftovers
+Ability: Levitate
+Tera Type: Poison
+EVs: 252 HP / 176 Def / 80 SpD
+Calm Nature
+IVs: 0 Atk / 0 Spe
+- Moonblast
+- Moonlight
+- Trick Room
+- Lunar Dance
+
+Iron Hands @ Booster Energy
+Ability: Quark Drive
+Tera Type: Flying
+EVs: 252 HP / 252 Atk / 4 SpD
+Adamant Nature
+- Wild Charge
+- Drain Punch
+- Ice Punch
+- Swords Dance
+
+Kingambit @ Leftovers
+Ability: Supreme Overlord
+Tera Type: Flying
+EVs: 252 HP / 252 Atk / 4 SpD
+Adamant Nature
+- Kowtow Cleave
+- Iron Head
+- Sucker Punch
+- Swords Dance""",
+]
